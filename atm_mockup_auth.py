6f17ab212c9e6d39ffb9813c96497f5ed8c4c499
Jessica Lam <49288633+jesslam@users.noreply.github.com>
2021-04-25 18:32:09 -0700

    Submit ATM Mock Up with File System as Database assignment incl user log, update current balance dynamically

--- atm_mockup_auth.py
+++ atm_mockup_auth.py
@@ -5,14 +5,8 @@
 import random
 import validation
 import database
+from getpass import getpass
 #dateTime = datetime.datetime.now()
-
-""" database = {
-    7891789: ['Seyi', 'Super', 'seyi@email.com', 'passwordSeyi', 8620],
-    4856947: ['Mike', 'Duper', 'mike@email.com', 'passwordMike', 34],
-    2559367: ['Love', 'Rumer', 'love@email.com', 'passwordLove', 956]
-    } """
-
 
 def init():
     print("~~~Welcome to TNT ATM~~~")
@@ -21,25 +15,42 @@ def init():
         login()
     elif acct_exist_ask == 'N':
         register()
+    else:
+        print('Invalid option')
+        init()
 
 def login():
-    user_acct_entered = input('Please enter your account number: ')
-    is_acct_num_valid = validation.acct_num_validation(user_acct_entered)
+    print('----- L o g  i n -----')
+    acct_num = input('Please enter your account number: ')
+    is_acct_num_valid = validation.acct_num_validation(acct_num)
 
     if is_acct_num_valid:
-        user_pass_entered = input('Please enter your password: ')
-        for acct_num, user_details in database.items():
-            if acct_num == int(user_acct_entered):
-                if user_details[3] == user_pass_entered:
-                    print('Login Successful. \n')
-                    bankingOptions(user_details)
-
-        print('Invalid account or password')
-        login()
-    
+        pass_entered = getpass('Please enter your password: ')
+        auth_user = database.authenticated_user(acct_num, pass_entered)
+        #print(auth_user)
+        if auth_user:
+            database.create_user_log(acct_num)
+            bankingOptions(acct_num, auth_user)
+        else:
+            print('Invalid account or password')
+            invalid_login = int(input('\nSelect one:\n1. Retry Login\n2. Register\n'))
+            if (invalid_login == 1):
+                login()
+            elif invalid_login == 2:
+                register()
+            else:
+                print('Please try again')
+                init()
     else:
         print('Account number invalid') 
         init()
+
+"""         for acct_num, user_details in database.items():
+            if acct_num == int(user_acct_entered):
+                if user_details[3] == user_pass_entered:
+                    print('Login Successful. \n')
+                    bankingOptions(user_details) """
+
 """                 else:
                     print('Your password is incorrect.')
                     exit()
@@ -53,15 +64,14 @@ def register():
     email = input('Email address: \n')
     first_name = input('First Name: \n')
     last_name = input('Last Name: \n')
-    password = input('Password: \n')
-    balance = 0
+    password = getpass('Password: \n')
 
     acct_num = createAcctNum()
     
     #print('\nThe current date and time is: ')
     #print(dateTime, end="\n\n")
     #database[acct_num] = [first_name, last_name, password, email, balance]
-    check_for_acct = database.create(acct_num, [first_name, last_name, password, email, balance])
+    check_for_acct = database.create(acct_num, first_name, last_name, password, email)
 
     # Use the database module to create a new user record
     # Create a file
@@ -73,6 +83,34 @@ def register():
         print('Something went wrong, please try again')
         register()
 
+def bankingOptions(acct_num, user_info):
+    print("These are the available options:")
+    print("1. Withdrawal")
+    print("2. Deposit")
+    print("3. Complaint")
+    print("4. Log out")
+    
+    selectedOption = int(input("Please select an option: ")) 
+    # this needed to be converted to an int because input defaulted to a str
+    if (selectedOption == 1):
+        withdrawal(acct_num, user_info)
+
+    elif (selectedOption == 2):
+        deposit(acct_num, user_info)
+
+    elif (selectedOption == 3):
+        print(input("\nWhat issue would you like to report? "))
+        print("Thank you, we appreciate you contacting us.\n")
+        bankingOptions(acct_num, user_info)
+
+    elif (selectedOption == 4):
+        database.delete_user_log(acct_num)
+        print("Thank you, goodbye.\n")
+        exit()
+            
+    else:
+        print("Invalid option selected, please try again.\n")
+        bankingOptions(acct_num, user_info)
 
 def createAcctNum():
     return random.randrange(1000000, 9999999)
@@ -80,52 +118,36 @@ def createAcctNum():
 def getAcctNum():
     return database.key()
 
-def deposit(user):
+def deposit(acct_num, user_info):
     print('++++Deposit++++')
-    # check the user's current balance and add the deposit
-    user_acct_balance = user[4]
+    user_acct_balance = int(user_info[4])
     print('Your current balance is: $%d' % user_acct_balance)
-    deposit_amt = int(input("\nHow much would you like to deposit? "))
+    deposit_amt = int(input("How much would you like to deposit? "))
     user_acct_balance += deposit_amt
-    print("Your deposit of $%d has been accepted. Your new balance is: $%d" % (deposit_amt,user_acct_balance))
-    bankingOptions(user)
+    user_info[4] = user_acct_balance
+    user_info = user_info[0] + ',' + user_info[1] + ','+ user_info[2] + ','+user_info[3] + ',' + str(user_info[4])
+    database.update(acct_num, user_info)
+    user_info = user_info.split(',')
+    print("\nYour deposit of $%d has been accepted. Your new balance is: $%d\n" % (deposit_amt,user_acct_balance))    
+    bankingOptions(acct_num, user_info)
 
-def withdrawal(user):
+def withdrawal(acct_num, user_info):
     print('----Withdrawal----')
-    user_acct_balance = user[4]
+    user_acct_balance = int(user_info[4])
     print('Your current balance is: $%d' % user_acct_balance)
-    withdraw_amt = int(input("\nHow much would you like to withdraw? "))
-    user_acct_balance -= withdraw_amt
-    print('Your new balance is $%d. Please take your cash totaling: $%s' % (user_acct_balance,withdraw_amt))
-    bankingOptions(user)
-
-def bankingOptions(user):
-    print("These are the available options:")
-    print("1. Withdrawal")
-    print("2. Deposit")
-    print("3. Complaint")
-    print("4. Exit")
-    
-    selectedOption = int(input("Please select an option: ")) 
-    # this needed to be converted to an int because input defaulted to a str
-    if (selectedOption == 1):
-        withdrawal(user)
-
-    elif (selectedOption == 2):
-        deposit(user)
-
-    elif (selectedOption == 3):
-        print(input("\nWhat issue would you like to report? "))
-        print("Thank you, we appreciate you contacting us.\n")
-        bankingOptions(user)
-
-    elif (selectedOption == 4):
-        print("Thank you, goodbye.\n")
-        exit()
-            
+    withdraw_amt = int(input("How much would you like to withdraw? "))
+    if withdraw_amt > user_acct_balance:
+        print('\nInsufficient funds, transaction cannot be completed')
     else:
-        print("Invalid option selected, please try again.\n")
-        bankingOptions(user)
+        user_acct_balance -= withdraw_amt
+        user_info[4] = user_acct_balance
+        user_info = user_info[0] + ',' + user_info[1] + ','+ user_info[2] + ','+user_info[3] + ',' + str(user_info[4])
+        database.update(acct_num, user_info)
+        user_info = user_info.split(',')
+        print('\nYour new balance is $%d. Please take your cash totaling: $%s\n' % (user_acct_balance,withdraw_amt))
+    bankingOptions(acct_num, user_info)
+
+
 
 init()
 
--- database.py
+++ database.py
@@ -6,24 +6,29 @@
 
 import os
 import validation
+import datetime
 user_db_path = 'atm-data/user_record/'
+user_log_path = 'atm-data/auth_session/'
+dateTime = datetime.datetime.now()
 # Find user by account number key
 
-# Create a file
-# Name of the file would be {acct_num}.txt
-# Add the user details to the file
-# Return true
-def create(acct_num, user_details):
+# Create a file <acct_num>.txt
+# Add the user details to the file, return true
+# If saving to file fails, check file contents and 
+# delete the file that was created via acct_num
+
+def create(acct_num, first_name, last_name, password, email):
+    user_details = first_name + ',' + last_name + ',' + password + ',' + email + ',' + str(0)
     print('Create a new record')
+    if doesEmailExist(email):
+        return False
     completion_state = False
     try:
         f = open(user_db_path + str(acct_num) + '.txt', 'x')
     except FileExistsError:
         print('User already exists')
-        # If saving to file fails, delete the file that was created
-        # check file contents before deleting
-        # delete(acct_num)
-        return False
+        if is_file_empty(acct_num) == True:
+            delete(acct_num)
     else:
         f.write(str(user_details))
         completion_state = True
@@ -32,13 +37,8 @@ def create(acct_num, user_details):
         return completion_state
 
 
-def update(acct_num):
-    print('Update user record')
 # Find user by account number
 # Fetch contents of the file
-# Update file content
-# Save file
-
 def read(acct_num):
     #print('Read user record')
     is_valid_acct_num = validation.acct_num_validation(acct_num)
@@ -56,8 +56,26 @@ def read(acct_num):
         print('Invalid account number format')
     else:
         return (f.readline())
+
+
 # Find user by account number
 # Fetch contents of the file
+# Update file content
+# Save file
+
+def update(acct_num, user_info):
+    if doesAcctExist(acct_num):
+        f = open(user_db_path + str(acct_num) + '.txt', 'w')
+        f.write(user_info)
+        f.close()
+        return True
+    print('record not updated')
+    return False
+
+
+# Find user by account number
+# Delete user record (file)
+# Return true if deletion succeeds
 
 def delete(acct_num):
     print('Delete user record')
@@ -71,17 +89,71 @@ def delete(acct_num):
         finally:
             return is_delete_successful 
     return True
-        
-# Find user by account number
-# Delete user record (file)
-# Return true if deletion succeeds
 
-def doesEmailExist(acct_num, email):
+
+# When user login succeeds, create file and track
+# When user logs out, delete log file that was created
+
+def create_user_log(acct_num):
+    try:
+        f = open(user_log_path + str(acct_num) + '.txt', 'x')
+        f.write('Account number <' + acct_num + '> last logged in successfully at ' + str(dateTime) + '.')
+    except FileExistsError:
+        delete_user_log(acct_num)
+        print('A duplicate session has been detected.  You have been logged out as a precaution, please log in again.')
+        exit()
+
+def delete_user_log(acct_num):
+    if os.path.exists(user_log_path + str(acct_num) + '.txt'):
+        try:
+            os.remove(user_log_path + str(acct_num) + '.txt')
+        except FileNotFoundError:
+            print('No active session found')
+
+def doesEmailExist(email):
     #print('Find user')
     all_users = os.listdir(user_db_path)
     for user in all_users:
-        print(read(user))
+        user_list = str.split(read(user), ',')
+        if email in user_list:
+            return True
+    return False
 
+def doesAcctExist(acct_num):
+    all_users = os.listdir(user_db_path)
+    for user in all_users:
+        if user == str(acct_num) + '.txt':
+            return True
+
+def is_file_empty(acct_num):
+    #print(os.stat(user_db_path + str(acct_num) + '.txt'))
+    if os.stat(user_db_path + str(acct_num) + '.txt').st_size == 0:
+        return True
+
+def authenticated_user(acct_num, password):
+    if doesAcctExist(acct_num):
+        user_info = str.split(read(acct_num),',')
+        if password == user_info[2]:
+            return user_info
+    return False
+
+    
+"""     all_users = os.listdir(user_db_path)
+    for user in all_users:
+        if email in (read(user)):
+            print('first')
+            return True
+    return False """
+
+""" def doesAcctExist(acct_num):
+    #print('Find user')
+    all_users = os.listdir(user_db_path)
+    for user in all_users:
+        if user == str(acct_num) + '.txt':
+            print('here')
+            print(user)
+            #return True
+    return False """
 # Find user record in the data folder
 
-doesEmailExist(2714355, 'heres@email.com')
+#doesEmailExist(2714355, 'secondex@email.com')
